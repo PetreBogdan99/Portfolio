6467541fdf60f3fa6d8c5d755db254a4a3d1a932
petrebogdan99 <petre.bogdan1@yahoo.com>
2021-02-19 03:13:04 +0200

    js update

--- cvscript.js
+++ cvscript.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Developer"],
+    strings: ["Curriculum Vitae"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
